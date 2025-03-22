7999444b5b618fea598e7bc564bfafd6f5870ab4
MariaMayskaya <mariamayskaya@mail.ru>
2025-03-22 16:36:42 +0300

    Add files via upload

---
+++ рыба/Program2.cs
@@ -0,0 +1,113 @@
+﻿
+//Упражнение 1 (Цикл for)
+
+//using System;
+//class Program
+//{
+//    static void Main(string[] args)
+//    {
+
+//        Console.Write("Введите сумму вклада: ");
+//        double principal = Convert.ToDouble(Console.ReadLine());
+
+//        Console.Write("Введите количество месяцев: ");
+//        int months = Convert.ToInt32(Console.ReadLine());
+
+//        double interestRate = 0.07;
+
+//        double finalAmount = principal;
+
+//        for (int i = 0; i < months; i++)
+//        {
+//            finalAmount += finalAmount * interestRate;
+//        }
+
+//        Console.WriteLine($"Конечная сумма вклада через {months} месяцев составит: {finalAmount:F2}");
+//    }
+//}
+
+
+
+//Упражнение 2 (Цикл while)
+
+//using System;
+
+//using System;
+//class Program
+//{
+//    static void Main(string[] args)
+//    {
+
+//        Console.Write("Введите сумму вклада: ");
+//        double principal = Convert.ToDouble(Console.ReadLine());
+
+//        Console.Write("Введите количество месяцев: ");
+//        int months = Convert.ToInt32(Console.ReadLine());
+
+
+//        double interestRate = 0.07;
+//        double totalAmount = principal;
+//        int monthCounter = 0;
+
+
+//        while (monthCounter < months)
+//        {
+//            totalAmount += totalAmount * interestRate;
+//            monthCounter++;
+//        }
+//        Console.WriteLine($"Конечная сумма вклада через {months} месяцев: {totalAmount:F2}");
+//    }
+//}
+
+
+
+////Упражнение 3 (Таблица умножения)
+
+//using System;
+
+//public class MultiplicationTable
+//{
+//    public static void Main(string[] args)
+//    {
+//        Console.WriteLine("Таблица умножения:");
+//        for (int i = 1; i <= 10; i++)
+//        {
+//            for (int j = 1; j <= 10; j++)
+//            {
+//                Console.Write(i * j + "\t"); 
+//            }
+//            Console.WriteLine(); 
+//        }
+//    }
+//}
+
+
+////Упражнение 4 (Проверка диапазона с while и break)
+
+//using System;
+
+//public class MultiplicationRange
+//{
+//    public static void Main(string[] args)
+//    {
+//        while (true)
+//        {
+//            Console.WriteLine("Введите первое число (от 0 до 10):");
+//            int num1 = Convert.ToInt32(Console.ReadLine());
+
+//            Console.WriteLine("Введите второе число (от 0 до 10):");
+//            int num2 = Convert.ToInt32(Console.ReadLine());
+
+//            if (num1 < 0 || num1 > 10 || num2 < 0 || num2 > 10)
+//            {
+//                Console.WriteLine("Введенные числа недопустимы. Пожалуйста, попробуйте еще раз.");
+//            }
+//            else
+//            {
+//                Console.WriteLine("Результат умножения: " + num1 * num2);
+//                break;
+//            }
+//        }
+//    }
+//}
+
---
+++ рыба/Program3.cs
@@ -0,0 +1,47 @@
+﻿//Вопрос 1: 18
+//Вопрос 2: 2 тк запятая в int[,] указывает на двумерный массив.
+
+//Вопрос 3: Программа завершится с ошибкой (IndexOutOfRangeException). Вы пытаетесь обратиться к элементу nums[3][2], но массив nums имеет только 3 элемента (индексы 0, 1, 2).
+//Вопрос 4: nums[2][1]. nums[2] обращается к третьему элементу массива nums (который сам является массивом { 6, 7, 8, 9, 10 }), а nums[2][1] обращается ко второму элементу этого внутреннего массива (индекс 1), который равен 7.
+
+//Практическое задание:
+//using System;
+//public class Program
+//{
+//    public static void Main(string[] args)
+//    {
+//        int[,,] mas = { { { 1, 2 }, { 3, 4 } },
+//                        { { 4, 5 }, { 6, 7 } },
+//                        { { 7, 8 }, { 9, 10 } },
+//                        { { 10, 11 }, { 12, 13 } }
+//                      };
+
+//        for (int i = 0; i < mas.GetLength(0); i++) 
+//        {
+//            Console.Write("{{");
+//            for (int j = 0; j < mas.GetLength(1); j++) 
+//            {
+//                Console.Write("{");
+//                for (int k = 0; k < mas.GetLength(2); k++) 
+//                {
+//                    Console.Write(mas[i, j, k]);
+//                    if (k < mas.GetLength(2) - 1)
+//                    {
+//                        Console.Write(" , ");
+//                    }
+//                }
+//                Console.Write("}");
+//                if (j < mas.GetLength(1) - 1)
+//                {
+//                    Console.Write(" , ");
+//                }
+//            }
+//            Console.Write("}}");
+//             if (i < mas.GetLength(0) - 1)
+//             {
+//                 Console.Write(" , ");
+//             }
+//        }
+//    }
+//}
+
