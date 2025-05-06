220b1eec6c71162675bbb0146d7b98490d97d907
MariaMayskaya <mariamayskaya@mail.ru>
2025-05-06 19:19:55 +0300

    Add files via upload

---
+++ рыба/calc.cs
@@ -0,0 +1,55 @@
+using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double number1;
+            double number2;
+
+            char operation;
+
+            Console.WriteLine("Привет! Это простой калькулятор.");
+
+            Console.Write("Введите первое число: ");
+            string input1 = Console.ReadLine(); 
+            number1 = Convert.ToDouble(input1); 
+
+            Console.Write("Введите второе число: ");
+            string input2 = Console.ReadLine(); 
+            number2 = Convert.ToDouble(input2); 
+  
+            Console.Write("Выберите операцию (+, -, *, /, %): ");
+            operation = Convert.ToChar(Console.ReadLine()); 
+
+            double result = 0; 
+
+            if (operation == '+')
+            {
+                result = number1 + number2;
+            }
+            if (operation == '-')
+            {
+                result = number1 - number2;
+            }
+            if (operation == '*')
+            {
+                result = number1 * number2;
+            }
+            if (operation == '/')
+            {
+                result = number1 / number2; 
+            }
+            if (operation == '%')
+            {
+                result = number1 % number2;  
+            }
+
+            Console.WriteLine("Результат: " + result);
+
+        }
+    }
+}
